056d9d0aec78a40aabc531b7b4844ecbeb9982ce
Voxaire <voxairegaming16@gmail.com>
2023-02-13 21:07:15 -0500

    Setting up

---
+++ Basics/src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+    //Variables must be definied then assigned a value. Here is an example of defining a Variable then giving it a value later
+    int x;
+    x = 20; //The Variable x is defined as an integer data-type, then is given the value 20
+    //This is an example of a variable being defined then assigned a value in the same line.
+    int y = 30;
+
+    //Data-Types
+    int a = 10;
+
+    }
+}
